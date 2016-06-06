afe1d075398ba75837296b0e1e0d9bac7fa24eb0
Tim Caswell <tim@creationix.com>
2016-06-06 10:23:06 -0500

    Start implementing TCP methods

--- app/main.js
+++ app/main.js
@@ -10,14 +10,15 @@ var uv = nucleus.uv;
 print("\nTimer.prototype");
 p(uv.Timer.prototype);
 print("Handle.prototype (via Timer.prototype)");
-p(uv.Timer.prototype.__proto__);
+p(Object.getPrototypeOf(uv.Timer.prototype));
 
 print("\nTcp.prototype");
 p(uv.Tcp.prototype);
 print("Stream.prototype (via Tcp.prototype)");
-p(uv.Tcp.prototype.__proto__);
+var streamProto = Object.getPrototypeOf(uv.Tcp.prototype);
+p(streamProto);
 print("Handle.prototype (via Stream.prototype)");
-p(uv.Tcp.prototype.__proto__.__proto__);
+p(Object.getPrototypeOf(streamProto));
 
 print("\nTesting uv.walk");
 var timer = new uv.Timer();
@@ -28,7 +29,7 @@ tcp.close();
 
 print("\nTesting simple timeout");
 var timer = new uv.Timer();
-timer.start(1000, 0, function () {
+timer.start(100, 0, function () {
   p("timeout", timer);
   timer.close(function () {
     p("closed", timer);
@@ -39,7 +40,7 @@ uv.run();
 print("\nTesting simple interval");
 timer = new uv.Timer();
 var count = 3;
-timer.start(500, 500, function () {
+timer.start(50, 50, function () {
   p("interval", timer);
   if (--count) return;
   timer.close(function () {
@@ -52,12 +53,12 @@ uv.run();
 print("\nTest two concurrent timers");
 var timer1 = new uv.Timer();
 var timer2 = new uv.Timer();
-timer1.start(1000, 0, function () {
+timer1.start(100, 0, function () {
   p("timeout", timer1);
   timer1.close();
   timer2.close();
 });
-timer2.start(100, 200, function () {
+timer2.start(10, 20, function () {
   p("interval", timer2);
 });
 uv.run();
@@ -68,7 +69,7 @@ timer.start(10, 0, function () {
   var repeat = timer.getRepeat();
   p("interval", timer, repeat);
   if (repeat === 0) {
-    timer.setRepeat(32);
+    timer.setRepeat(8);
     timer.again();
   }
   else if (repeat == 2) {
@@ -79,6 +80,30 @@ timer.start(10, 0, function () {
     timer.setRepeat(repeat / 2);
   }
 });
+uv.run();
+
+print("\nTesting TCP Server");
+var server = new uv.Tcp();
+server.bind("127.0.0.1", 8080);
+server.listen(128, function (err) {
+  if (err) throw err;
+  var client = new uv.Tcp();
+  server.accept(client);
+  p("client", client, client.getpeername());
+  client.readStart(function (err, chunk) {
+    if (err) throw err;
+    if (chunk) {
+      client.write(chunk);
+    }
+    else {
+      client.shutdown(function () {
+        client.close();
+        server.close();
+      });
+    }
+  });
+});
+p("server", server, server.getsockname());
 uv.run();
 
 print("\nTesting scandir at root");
--- implementations/duktape/duv/tcp.c
+++ implementations/duktape/duv/tcp.c
@@ -9,23 +9,75 @@ duk_ret_t duv_tcp(duk_context *ctx) {
 }
 
 duk_ret_t duv_tcp_open(duk_context *ctx) {
-  duk_error(ctx, DUK_ERR_UNIMPLEMENTED_ERROR, "TODO: Implement duv_tcp_open");
+  dschema_check(ctx, (const duv_schema_entry[]) {
+    {"fd", duk_is_number},
+    {0,0}
+  });
+  uv_tcp_t *tcp = duv_require_this_handle(ctx, DUV_TCP_MASK);
+  duv_check(ctx, uv_tcp_open(tcp,
+    duk_get_int(ctx, 1)
+  ));
+  return 0;
 }
 
 duk_ret_t duv_tcp_nodelay(duk_context *ctx) {
-  duk_error(ctx, DUK_ERR_UNIMPLEMENTED_ERROR, "TODO: Implement duv_tcp_nodelay");
+  dschema_check(ctx, (const duv_schema_entry[]) {
+    {"enable", duk_is_boolean},
+    {0,0}
+  });
+  uv_tcp_t *tcp = duv_require_this_handle(ctx, DUV_TCP_MASK);
+  duv_check(ctx, uv_tcp_nodelay(tcp,
+    duk_get_int(ctx, 1)
+  ));
+  return 0;
 }
 
 duk_ret_t duv_tcp_keepalive(duk_context *ctx) {
-  duk_error(ctx, DUK_ERR_UNIMPLEMENTED_ERROR, "TODO: Implement duv_tcp_keepalive");
+  dschema_check(ctx, (const duv_schema_entry[]) {
+    {"enable", duk_is_boolean},
+    {"delay", duk_is_number},
+    {0,0}
+  });
+  uv_tcp_t *tcp = duv_require_this_handle(ctx, DUV_TCP_MASK);
+  duv_check(ctx, uv_tcp_keepalive(tcp,
+    duk_get_int(ctx, 1),
+    duk_get_int(ctx, 2)
+  ));
+  return 0;
 }
 
 duk_ret_t duv_tcp_simultaneous_accepts(duk_context *ctx) {
-  duk_error(ctx, DUK_ERR_UNIMPLEMENTED_ERROR, "TODO: Implement duv_tcp_simultaneous_accepts");
+  dschema_check(ctx, (const duv_schema_entry[]) {
+    {"enable", duk_is_boolean},
+    {0,0}
+  });
+  uv_tcp_t *tcp = duv_require_this_handle(ctx, DUV_TCP_MASK);
+  duv_check(ctx, uv_tcp_simultaneous_accepts(tcp,
+    duk_get_int(ctx, 1)
+  ));
+  return 0;
 }
 
 duk_ret_t duv_tcp_bind(duk_context *ctx) {
-  duk_error(ctx, DUK_ERR_UNIMPLEMENTED_ERROR, "TODO: Implement duv_tcp_bind");
+  dschema_check(ctx, (const duv_schema_entry[]) {
+    {"host", duk_is_string},
+    {"port", duk_is_number},
+    {0,0}
+  });
+  uv_tcp_t *tcp = duv_require_this_handle(ctx, DUV_TCP_MASK);
+  const char *host = duk_get_string(ctx, 1);
+  int port = duk_get_number(ctx, 2),
+      flags = 0;
+  struct sockaddr_storage addr;
+  if (uv_ip4_addr(host, port, (struct sockaddr_in*)&addr) &&
+      uv_ip6_addr(host, port, (struct sockaddr_in6*)&addr)) {
+    duk_error(ctx, DUK_ERR_TYPE_ERROR, "Invalid IP address or port");
+  }
+  duv_check(ctx, uv_tcp_bind(tcp,
+    (struct sockaddr*)&addr,
+    flags
+  ));
+  return 0;
 }
 
 duk_ret_t duv_tcp_getpeername(duk_context *ctx) {
