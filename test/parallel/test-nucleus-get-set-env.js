7b276f982ef0e143c4dd302e84d5a22286ddd5fa
Jeremiah Harlan <jeremiah.harlan@gmail.com>
2016-06-15 23:58:17 -0400

    Update assert messages

--- test/parallel/test-nucleus-get-set-env.js
+++ test/parallel/test-nucleus-get-set-env.js
@@ -4,28 +4,28 @@ var assert = nucleus.dofile("./common/assert.js")
 assert(nucleus.getenv('__NUCLEUS_TEST') === null, 'nonexisting env var should be `null`')
 
 // set new
-assert(nucleus.setenv('__NUCLEUS_TEST', 'nice day @nodeconf') === undefined, 'created env should return `undefined`')
+assert(nucleus.setenv('__NUCLEUS_TEST', 'nice day @nodeconf') === undefined, 'setenv should return `undefined`')
 
 // get newly set
-assert(nucleus.getenv('__NUCLEUS_TEST') === 'nice day @nodeconf', 'get existing env should return value')
+assert(nucleus.getenv('__NUCLEUS_TEST') === 'nice day @nodeconf', 'getenv should return value')
 
 // newly set in envkeys
-assert(nucleus.envkeys().indexOf('__NUCLEUS_TEST') !== -1, 'existing key index should not equal -1')
+assert(nucleus.envkeys().indexOf('__NUCLEUS_TEST') !== -1, 'env variable should be set in envkeys')
 
 // re-set
-assert(nucleus.setenv('__NUCLEUS_TEST', 'hi there') === undefined, 'setting existing env should return `undefined`')
+assert(nucleus.setenv('__NUCLEUS_TEST', 'hi there') === undefined, 'setenv should return `undefined`')
 
 // get re-set
 assert(nucleus.getenv('__NUCLEUS_TEST') === 'hi there', 'get existing key should return `hi there`')
 
 // re-set in envkeys
-assert(nucleus.envkeys().indexOf('__NUCLEUS_TEST') !== -1, 'existing key index should not equal -1')
+assert(nucleus.envkeys().indexOf('__NUCLEUS_TEST') !== -1, 'env variable should be set in envkeys')
 
 // unset
-assert(nucleus.unsetenv('__NUCLEUS_TEST') === undefined, 'unsetting env should return `undefined`')
+assert(nucleus.unsetenv('__NUCLEUS_TEST') === undefined, 'setenv should return `undefined`')
 
 // get now non-existing
 assert(nucleus.getenv('__NUCLEUS_TEST') === null, 'nonexisting env var should be `null`')
 
 // no longer in envkeys
-assert(nucleus.envkeys().indexOf('__NUCLEUS_TEST') === -1, 'nonexisting key index should equal -1')
+assert(nucleus.envkeys().indexOf('__NUCLEUS_TEST') === -1, 'env variable should be set in envkeys')
